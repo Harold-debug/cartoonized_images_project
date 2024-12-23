eb8f47133b5cc02e648f167763eff6a4b939a402
Harold Geumtcheng <geumtchengh@gmail.com>
2024-12-23 17:17:07 +0100

    Add Pix2Pix model components including generator and discriminator

---
+++ models.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from tensorflow import keras
+
+
+# ----------------------------
+#     PIX2PIX COMPONENTS
+# ----------------------------
+
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
+
+def downsample(filters, size, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    result = tf.keras.Sequential()
+    result.add(layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer=initializer, use_bias=not apply_batchnorm))
+    if apply_batchnorm:
+        result.add(layers.BatchNormalization())
+    result.add(layers.LeakyReLU())
+    return result
+
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    result = tf.keras.Sequential()
+    result.add(layers.Conv2DTranspose(filters, size, strides=2, padding='same',
+                                     kernel_initializer=initializer, use_bias=False))
+    result.add(layers.BatchNormalization())
+    if apply_dropout:
+        result.add(layers.Dropout(0.5))
+    result.add(layers.ReLU())
+    return result
+
+def build_generator():
+    inputs = layers.Input(shape=[256, 256, 3])
+
+    down_stack = [
+        downsample(64, 4, apply_batchnorm=False),
+        downsample(128, 4),
+        downsample(256, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+        downsample(512, 4),
+    ]
+
+    up_stack = [
+        upsample(512, 4, apply_dropout=True),
+        upsample(512, 4, apply_dropout=True),
+        upsample(512, 4, apply_dropout=True),
+        upsample(512, 4),
+        upsample(256, 4),
+        upsample(128, 4),
+        upsample(64, 4),
+    ]
+
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = layers.Conv2DTranspose(3, 4, strides=2,
+                                  padding='same',
+                                  kernel_initializer=initializer,
+                                  activation='tanh')
+
+    x = inputs
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+    skips = reversed(skips[:-1])
+
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = layers.Concatenate()([x, skip])
+
+    x = last(x)
+    return Model(inputs=inputs, outputs=x)
+
+def build_discriminator():
+    """PatchGAN discriminator for Pix2Pix."""
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    inp = layers.Input(shape=[IMG_HEIGHT, IMG_WIDTH, 3], name='input_image')
+    tar = layers.Input(shape=[IMG_HEIGHT, IMG_WIDTH, 3], name='target_image')
+
+    x = layers.Concatenate()([inp, tar])  # (bs, 256, 256, 6)
+
+    down1 = downsample(64, 4, apply_batchnorm=False)(x)  # (bs, 128, 128, 64)
+    down2 = downsample(128, 4)(down1)                     # (bs, 64, 64, 128)
+    down3 = downsample(256, 4)(down2)                     # (bs, 32, 32, 256)
+
+    zero_pad1 = layers.ZeroPadding2D()(down3)             # (bs, 34, 34, 256)
+    conv = layers.Conv2D(512, 4, strides=1, 
+                         kernel_initializer=initializer, 
+                         use_bias=False)(zero_pad1)        # (bs, 31, 31, 512)
+    batchnorm1 = layers.BatchNormalization()(conv)
+    leaky_relu = layers.LeakyReLU()(batchnorm1)
+
+    zero_pad2 = layers.ZeroPadding2D()(leaky_relu)         # (bs, 33, 33, 512)
+    last = layers.Conv2D(1, 4, strides=1,
+                         kernel_initializer=initializer)(zero_pad2)  
+    # (bs, 30, 30, 1)
+
+    return keras.Model(inputs=[inp, tar], outputs=last)
